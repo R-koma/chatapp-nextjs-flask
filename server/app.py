55a4e047d0632d766ccf40be6d20c0597ced28e1
R-koma <ryomakomazawa@gmail.com>
2024-06-14 22:14:36 +0900

    Prepare to connect with the client side

--- server/app.py
+++ server/app.py
@@ -1,13 +1,18 @@
 import os
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*") # TODO: ポート番号設定
+
+@socketio.on('message')
+def handle_message(msg):
+  print('Message: ' + msg)
+  send(msg, broadcast=True)
 
 if __name__ == '__main__':
-  socketio.run(app)
+  socketio.run(app, debug=True)
